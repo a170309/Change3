9060629be4d181bcc6e7cdbb6e0749989cc1065b
user <170309@naver.com>
2018-07-18 17:57:09 +0900

    Initial commit

---
+++ src/Change3.java
@@ -0,0 +1,23 @@
+public class Change3 {
+
+
+    public boolean change3(String s){
+        boolean answer = true;
+//
+//        if(s.length() == 4 || s.length() == 6){
+//            try{
+//                int a = Integer.parseInt(s);
+//                System.out.println(answer);
+//            }catch(Exception e){
+//                answer = false;
+//                System.out.println(answer);
+//            }
+//        }else
+//            answer = false;
+        return answer;
+
+
+    }
+
+
+}
